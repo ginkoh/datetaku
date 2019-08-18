3aa33d2b976859749c7de03bfaaa264e316d0672
Gian <gianszabernardo@gmail.com>
2019-08-18 16:41:51 -0300

    Add project files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import {Text} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <Text>Is athis b</Text>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
