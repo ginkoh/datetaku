2a9ada69d051397da2c348807a4a30e0e0a099f6
Gian <gianszabernardo@gmail.com>
2019-08-18 16:47:20 -0300

    Add some comments to the main file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
+// React Native.
 import {AppRegistry} from 'react-native';
-import App from './App';
+
+// App name.
 import {name as appName} from './app.json';
 
+// App main component.
+import App from './App';
+
+// Init the app.
 AppRegistry.registerComponent(appName, () => App);
