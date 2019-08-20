123d2a549c1ffa0e40348b8ff9883707d1952a4d
Gian <gianszabernardo@gmail.com>
2019-08-19 23:23:49 -0300

    Add white color to the app colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,8 @@
 const APP_COLORS = {
     MAIN_COLOR: "#761fb8",
     LOW_OPACITY_PURPLE: "#c4a9d9",
-    NORMAL_COLORS: "#9f4bc9"
+    NORMAL_COLORS: "#9f4bc9",
+    WHITE: '#FFF'
 };
 
 export default APP_COLORS;
